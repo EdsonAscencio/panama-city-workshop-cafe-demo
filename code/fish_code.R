c801b1cfbceb02c7963ff19b46dbd93479c0a851
Edson J Ascencio <edson.ascencio@upch.pe>
2026-05-12 09:24:25 -0500

    ceviche code

---
+++ code/fish_code.R
@@ -0,0 +1,16 @@
+# Ceviche Script
+# How to run: open this file in RStudio and click "Source",
+# or from the R console run: source("code/cupcake_code.R")
+
+library(tidyverse)
+
+
+make_ceviche <- function(n_fish = 1) {
+  ingredients <- c("fish", "fresh lemon", "chilly")
+
+  cat("Mixing:", paste(ingredients, collapse = ", "), "\n")
+  ceviche <- strrep("\U0001F988", n_fish)
+  print(paste0("Ceviche complete! ", ceviche))
+}
+
+make_ceviche(n_fish = 3)
